f525de6886a3586614d85e1b2fb360a022fce4e7
Santiago Cordero <santicordero28@gmail.com>
2025-05-01 20:14:55 -0600

    UI terminada junto con archivos de prueba

--- huffmantree/src/main/java/huffmantree/Driver.java
+++ huffmantree/src/main/java/huffmantree/Driver.java
@@ -3,7 +3,6 @@ package huffmantree;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -11,33 +10,62 @@ import java.util.Scanner;
  */
 public class Driver{
     public static void main( String[] args ){
-        boolean loop = true;
         Scanner input = new Scanner(System.in);
         do{
             System.out.println("--------------------------------");
             System.out.println("\tÁrbol de Huffman");
             System.out.println("--------------------------------");
-            System.out.print("\n\nIngrese el nombre del archivo que desea comprimir (Asegúrese que esté dentro de la carpeta 'huffmantree'): \nR/ ");
+            System.out.print("\nIngrese el nombre del archivo que desea comprimir (Asegúrese que esté dentro de la carpeta 'huffmantree', incluya también la extensión del archivo .txt) \n(Ingrese 's' para salir): \nR/ ");
             String fileName = input.nextLine();
-            ArrayList<String> data = new ArrayList<>();
+            if (fileName.equals("s")){
+                break;
+            }
+            StringBuilder data = new StringBuilder();
             try{
                 data = readFile(fileName);
             }catch (IOException E){
-                System.out.println("Hubo un error al leer el archivo, pruebe otra vez." + E.getMessage());
+                System.out.println("Hubo un error al leer el archivo, pruebe otra vez. Error: " + E.getMessage());
             }
 
-            HuffmanTree<String> ht = new HuffmanTree();
-        } while (loop);
+            String text = data.toString();
+            HuffmanTree ht = new HuffmanTree();
+            // ht.build(text);
+
+            // String encoded = ht.encode(text);
+            System.out.println("--------------------------------");
+            System.out.println("\t Texto codificado");
+            System.out.println("--------------------------------");
+            System.out.println(encoded);
+
+            // String decoded = ht.decode(encoded);
+            System.out.println("--------------------------------");
+            System.out.println("\t Texto decodificado");
+            System.out.println("--------------------------------");
+            System.out.println(decoded);
+
+            System.out.println("--------------------------------");
+            System.out.println("\t Tabla de caracteres");
+            System.out.println("--------------------------------");
+            
+            /*
+            HashMap<Character, String> m = (HashMap<Character, String>) ht.getCodes();
+            for (Character c : m.keySet()){
+                System.out.println("'" + c + "' : " + m.get(c));
+            }
+            */
+            
+        } while (true);
+        System.out.println("Gracias por utilizar nuestro código :D");
     }
 
-    public static ArrayList<String> readFile(String fileName) throws IOException {
+    public static StringBuilder readFile(String fileName) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         String line;
-        ArrayList<String> tokens = new ArrayList<>();
+        StringBuilder b = new StringBuilder();
         while((line = br.readLine()) != null) {
-            tokens.add(line);
+            b.append(line).append("\n");
         }
         br.close();
-        return tokens;
+        return b;
     }
 }
